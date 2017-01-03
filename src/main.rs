08f15ace719768c03017891799677faf1c70972e
Tom Cornell <tom.cornell@gmail.com>
2017-01-03 14:00:05 -0500

    Export CharDataIter to its own file
    Called 'chardata.rs'.

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,10 @@
 use std::io;
 use std::io::prelude::*;
 
+mod chardata;
+
 static IN_TOKEN: u8 = 1;
 static BTWN_TOKS: u8 = 0;
-static END_OF_STRING: char = '\0';
-
-
-#[derive(Debug)]
-struct CharData {
-    ch: char,
-    byte_offset: usize,
-    char_offset: usize,
-}
-
-/////////////////////////////////////////////////////////
-// Probably CharDataIter could be replaced by a clever
-// call to map() on the underlying char iterator...
-//
-struct CharDataIter<'a> {
-    char_stream: &'a mut Iterator<Item = char>,
-    byte_offset: usize,
-    char_offset: usize,
-    really_done: bool,
-}
-
-impl<'a> CharDataIter<'a> {
-    fn new(chs: &'a mut Iterator<Item = char>) -> Self {
-        CharDataIter {
-            char_stream: chs,
-            byte_offset: 0,
-            char_offset: 0,
-            really_done: false,
-        }
-    }
-}
-
-impl<'a> Iterator for CharDataIter<'a> {
-    type Item = CharData;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.char_stream.next() {
-            Some(c) => {
-                let result = CharData {
-                    ch: c,
-                    byte_offset: self.byte_offset,
-                    char_offset: self.char_offset,
-                };
-                self.char_offset += 1;
-                self.byte_offset += c.len_utf8();
-                Some(result)
-            },
-            None => {
-                if self.really_done {
-                    None
-                } else {
-                    // Special <end-of-string> marker
-                    self.really_done = true;
-                    Some (
-                        CharData {
-                            ch: END_OF_STRING,  // should be ignored!
-                            byte_offset: self.byte_offset,
-                            char_offset: self.char_offset,
-                        }
-                    )
-                }
-            }
-        }
-    }
-}
-//
-// CharDataIter
-/////////////////////////////////////////////////////////
 
 
 
@@ -78,6 +12,13 @@ impl<'a> Iterator for CharDataIter<'a> {
 // TokenIter
 //
 
+/*  TODO: Probably there should not be a String member here. 
+    We should either borrow a string slice from the original text,
+    or else leave it out, and provide some other facility for 
+    converting Tokens to Strings, given the underlying string.
+    In a full fledged parser, the parsed-document representation 
+    would handle that.
+ */
 #[derive(Debug)]
 struct Token {
     text: String,
@@ -98,13 +39,13 @@ impl Token {
 }
 
 struct TokenIter<'a> {
-    chdat_stream: &'a mut CharDataIter<'a>,
+    chdat_stream: &'a mut chardata::CharDataIter<'a>,
     curr_tok_offset: usize,
     state: u8,
 }
 
 impl<'a> TokenIter<'a> {
-    fn new(chdats: &'a mut CharDataIter<'a>) -> Self {
+    fn new(chdats: &'a mut chardata::CharDataIter<'a>) -> Self {
         TokenIter {
             chdat_stream: chdats,
             curr_tok_offset: 0,
@@ -113,7 +54,7 @@ impl<'a> TokenIter<'a> {
     }
 
     fn is_boundary_char(ch: char) -> bool {
-        if ch == END_OF_STRING {
+        if ch == chardata::END_OF_STRING {
             true
         } else if ch.is_whitespace() {
             true
@@ -130,7 +71,7 @@ impl<'a> TokenIter<'a> {
     under construction. Update the current token offset.
     Leave the resulting Token as the return value of next().
 
-    If the underlying CharDataIter yields END_OF_SENTENCE:
+    If the underlying chardata::CharDataIter yields END_OF_SENTENCE:
         IN_TOKEN --> ship the current token
         BTWN_TOKS --> return None
     In the first case, the next call to next() will immediately trigger
@@ -146,7 +87,7 @@ impl<'a> Iterator for TokenIter<'a> {
         loop {
             match self.chdat_stream.next() {
 
-                Some( CharData {ch, byte_offset, char_offset} ) => {
+                Some( chardata::CharData {ch, byte_offset, char_offset} ) => {
 
                     if TokenIter::is_boundary_char(ch) {
                         if self.state == IN_TOKEN {
@@ -199,7 +140,7 @@ fn main() {
 
     // Construct a tokenizer by adapting some more primitive iterators
     let mut chs = s.chars();
-    let mut chds = CharDataIter::new(&mut chs);
+    let mut chds = chardata::CharDataIter::new(&mut chs);
     let mut toks = TokenIter::new(&mut chds);
 
     // Run the tokenizer, dump debug info for each token:
